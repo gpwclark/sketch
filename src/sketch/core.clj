c63a798141dde53ca5658c41dc7e0009d50a4691
gpwclark <gpwclark@gmail.com>
2021-02-28 19:55:05 +0000

    control probability of drawing based on quadrant

--- src/sketch/core.clj
+++ src/sketch/core.clj
@@ -8,16 +8,9 @@
              :title "Sketch"
              :setup dynamic/setup
              :draw dynamic/draw
-             :size [1800 1200]
-             ;;:size [900 900]
-             ;;:features [:keep-on-top]
-             ; fun-mode.
-             ;;:middleware [m/fun-mode]
-             )
+             :size [1800 1200])
 
 (defn refresh []
   (use :reload 'sketch.dynamic)
   (.loop example))
-
-(refresh)
 
--- src/sketch/dynamic.clj
+++ src/sketch/dynamic.clj
@@ -4,6 +4,17 @@
             [clojure.pprint :as pprint])
   (:use [clojure.pprint]))
 
+(defn quadrant [x y]
+  "Idea is to specify a probability more circles will be drawn for given coords."
+  (let [x-orig 900
+        y-orig 600]
+    (cond
+      (and (> x x-orig) (< y y-orig)) 3.4
+      (and (< x x-orig) (< y y-orig)) 2.4
+      (and (< x x-orig) (> y y-orig)) 3.4
+      (and (> x x-orig) (> y y-orig)) 2.4
+      :else 0)))
+
 (defn draw-circles [fun x y radius]
   (fill (mod x 255) (mod y 255) (mod (* radius radius) 255) (+ 80 (mod (* x y) 54)))
   (fun x y (* 2 radius) ( * 2 radius))
@@ -14,7 +25,7 @@
                         {:x x :y (+ y (* 2 radius)) :radius new-radius}
                         {:x x :y (- y (* 2 radius)) :radius new-radius})]
       (doall (map (fn [circle] (let [{x :x y :y radius :radius} circle]
-                           (draw-circles fun x y radius))) (random-sample 0.8 actions))))))
+                           (draw-circles fun x y radius))) (random-sample (/ (quadrant x y) 4) actions))))))
 
 (defn setup []
   (background 255 255 255)
